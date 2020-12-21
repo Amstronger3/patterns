fdcb1a2ed30bc461176229ff8e7e06770d5166b0
Amstronger3 <amstronger3@gmail.com>
2020-12-21 15:31:35 +0200

    singleton

---
+++ creational/singleton.py
@@ -0,0 +1,14 @@
+class Singleton(object):
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+            return cls.instance
+        else:
+            return False
+
+
+s = Singleton()
+print("Object created", s)
+s1 = Singleton()
+print("Object created", s1)
